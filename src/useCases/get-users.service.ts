8060de348ce19f6a40577c842881b097ab9ae990
Rogerio Inacio <rogerio.inacio@u4crypto.com>
2023-09-01 01:48:33 -0300

    feat: add use case to get use

---
+++ src/useCases/get-users.service.ts
@@ -0,0 +1,15 @@
+import {UserRepositoryInterface} from "../database/repositories/user-repository.interface";
+import { UserModel } from "../models/user.model";
+import { GetUsersServiceInterface } from "./interfaces/get-users-service.interface";
+
+export class GetUsersService implements GetUsersServiceInterface {
+    private repository: UserRepositoryInterface;
+
+    constructor(repository: UserRepositoryInterface) {
+        this.repository = repository;
+    }
+
+    public execute(query: string): UserModel[] {
+        return this.repository.searchUsers(query);
+    }
+}
