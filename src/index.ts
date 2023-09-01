23b61c8da6c2dcc5a92f874863dbbc7943089b16
Rogerio Inacio <rogerio.inacio@u4crypto.com>
2023-09-01 01:47:26 -0300

    refactor: import routes

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,20 @@
 import { Server } from './server';
+import { AppController } from "./controllers/app.controller";
+import { UserController } from "./controllers/user.controller";
+import { UserRepository } from "./database/repositories/user.repository";
+import { GetUsersService } from "./useCases/get-users.service";
+import { AddUsersService } from "./useCases/add-users.service";
 
 const server = new Server();
+const appController = new AppController();
+
+const userRepository = new UserRepository();
+const getUsersService = new GetUsersService(userRepository);
+const addUserService = new AddUsersService(userRepository);
+const userController = new UserController(getUsersService, addUserService);
+
+server.setRoutes(appController.getRoutes());
+server.setRoutes(userController.getRoutes());
+
 server.setPort(3000)
 server.start();
