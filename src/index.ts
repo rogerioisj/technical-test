2cfd7d50b8db96b372d1f2391761ae8253fdb967
Rogerio Inacio <rogerio.inacio@u4crypto.com>
2023-09-01 22:31:31 -0300

    refactor: remove unnecessary controller

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,4 @@
 import { ExpressServer } from './expressServer';
-import { AppController } from "./controllers/app.controller";
 import { UserController } from "./controllers/user.controller";
 import { UserRepository } from "./database/repositories/user.repository";
 import { GetUsersService } from "./useCases/get-users.service";
@@ -7,7 +6,6 @@ import { AddUsersService } from "./useCases/add-users.service";
 import { ProcessCsvFileService } from "./useCases/process-csv-file.service";
 
 const server = new ExpressServer();
-const appController = new AppController();
 
 const userRepository = new UserRepository();
 const getUsersService = new GetUsersService(userRepository);
@@ -15,7 +13,6 @@ const addUserService = new AddUsersService(userRepository);
 const processCsvFileService = new ProcessCsvFileService(addUserService);
 const userController = new UserController(getUsersService, addUserService, processCsvFileService);
 
-server.setRoutes(appController.getRoutes(), '/api');
 server.setRoutes(userController.getRoutes(), '/api');
 
 server.setPort(3000)
