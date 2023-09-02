142827ca565386a463f95f88eed6f500a0e516e4
Rogerio Inacio <rogerio.inacio@u4crypto.com>
2023-09-01 23:01:00 -0300

    test: create unit test for csv process

---
+++ test/unit/process-csv-service.spec.ts
@@ -0,0 +1,34 @@
+import { describe, expect, test } from '@jest/globals';
+import { UserRepository } from "../../src/database/repositories/user.repository";
+import { AddUsersService } from "../../src/useCases/add-users.service";
+import { ProcessCsvFileService } from "../../src/useCases/process-csv-file.service";
+import { GetUsersService } from "../../src/useCases/get-users.service";
+import { copyFileSync, unlink } from "fs";
+
+describe('Given an instance of ProcessCsvFileService', () => {
+    let userRepository: UserRepository;
+    let addUserService: AddUsersService;
+    let getUsersService: GetUsersService;
+    let sut: ProcessCsvFileService;
+
+    beforeEach(() => {
+        userRepository = new UserRepository();
+        addUserService = new AddUsersService(userRepository);
+        getUsersService = new GetUsersService(userRepository);
+        sut = new ProcessCsvFileService(addUserService);
+    });
+
+    afterEach(() => {
+        userRepository = null as any;
+    });
+
+
+    test('Should save a list of users based on a csv file', async () => {
+        const filePathMock: string = 'test/mocks/users.csv';
+        const filePath: string = 'temp/users.csv';
+        copyFileSync(filePathMock, filePath)
+        await sut.execute(filePath);
+        const result = getUsersService.execute('');
+        expect(result.length).toBe(6);
+    });
+});
