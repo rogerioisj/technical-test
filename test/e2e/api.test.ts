0cbaebe59354b515cbc2e3b90fa6fe43151b6cad
Rogerio Inacio <rogerio.inacio@u4crypto.com>
2023-09-01 23:00:41 -0300

    test: skip unnecessary tests

--- test/e2e/api.test.ts
+++ test/e2e/api.test.ts
@@ -1,15 +1,27 @@
 import { describe, expect } from '@jest/globals';
 import supertest, { Test } from 'supertest';
 import { ExpressServer } from '../../src/expressServer';
-import { AppController } from "../../src/controllers/app.controller";
+import { UserController } from "../../src/controllers/user.controller";
+import { AddUsersService } from "../../src/useCases/add-users.service";
+import { GetUsersService } from "../../src/useCases/get-users.service";
+import { ProcessCsvFileService } from "../../src/useCases/process-csv-file.service";
+import { UserRepository } from "../../src/database/repositories/user.repository";
 
 describe('Given an API test e2e suit', () => {
     const serverInstance = new ExpressServer();
     let server: supertest.SuperTest<supertest.Test>;
     let port = 3001;
-    const appController = new AppController();
+
+    let sut: UserController;
+
     beforeEach(async () => {
-        serverInstance.setRoutes(appController.getRoutes());
+        const repository = new UserRepository();
+        const addUserService = new AddUsersService(repository);
+        const getUserService = new GetUsersService(repository);
+        const processCsvFileService = new ProcessCsvFileService(addUserService);
+        sut = new UserController(getUserService, addUserService, processCsvFileService);
+
+        serverInstance.setRoutes(sut.getRoutes());
         serverInstance.setPort(port);
         serverInstance.start();
         server = supertest(serverInstance.getApp());
@@ -18,19 +30,22 @@ describe('Given an API test e2e suit', () => {
     afterEach(() => {
         serverInstance.close();
     });
-    test.todo('Should return a list of users based on query params');
-    test.todo('Should save a list of users based on a csv file');
-    test('Should return a hello world message', async () => {
+
+    test.skip('Should return a hello world message', async () => {
         const response = await server.get('/');
 
         expect(response.status).toBe(200);
         expect(response.text).toBe('Hello World!');
     });
 
-    test('Should return a hello message', async () => {
+    test.skip('Should return a hello message', async () => {
         const response = await server.get('/2');
 
         expect(response.status).toBe(200);
         expect(response.text).toBe('Hello!');
     });
+
+    test.todo('Should save a list of users based on a csv file');
+
+    test.todo('Should return a list of users based on query params');
 });
