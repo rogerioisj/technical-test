ada18bafda6b22775e459ff3cdbefef9bc141974
Rogerio Inacio <rogerio.inacio@u4crypto.com>
2023-09-01 22:29:30 -0300

    feat: add service and his dependencies

--- src/index.ts
+++ src/index.ts
@@ -4,6 +4,7 @@ import { UserController } from "./controllers/user.controller";
 import { UserRepository } from "./database/repositories/user.repository";
 import { GetUsersService } from "./useCases/get-users.service";
 import { AddUsersService } from "./useCases/add-users.service";
+import { ProcessCsvFileService } from "./useCases/process-csv-file.service";
 
 const server = new ExpressServer();
 const appController = new AppController();
@@ -11,7 +12,8 @@ const appController = new AppController();
 const userRepository = new UserRepository();
 const getUsersService = new GetUsersService(userRepository);
 const addUserService = new AddUsersService(userRepository);
-const userController = new UserController(getUsersService, addUserService);
+const processCsvFileService = new ProcessCsvFileService(addUserService);
+const userController = new UserController(getUsersService, addUserService, processCsvFileService);
 
 server.setRoutes(appController.getRoutes(), '/api');
 server.setRoutes(userController.getRoutes(), '/api');
