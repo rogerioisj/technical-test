1cb62e3a364fe13157914cdfe438b094769c004a
Rogerio Inacio <rogerio.inacio@u4crypto.com>
2023-09-02 18:24:35 -0300

    feat: add limit and page

--- src/useCases/interfaces/get-users-service.interface.ts
+++ src/useCases/interfaces/get-users-service.interface.ts
@@ -1,5 +1,5 @@
 import { UserModel } from "../../models/user.model";
 
 export interface GetUsersServiceInterface {
-    execute(query: string): UserModel[];
+    execute(query: string, page?: number, limit?: number): UserModel[];
 }
