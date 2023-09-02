71eb7c2f1e68b49981b4df58947f17e3fd25b141
Rogerio Inacio <rogerio.inacio@u4crypto.com>
2023-09-02 18:23:55 -0300

    feat: add params to page and limit

--- src/database/repositories/user-repository.interface.ts
+++ src/database/repositories/user-repository.interface.ts
@@ -1,6 +1,6 @@
 import { UserModel } from "../../models/user.model";
 
 export interface UserRepositoryInterface {
-    searchUsers(query: string): UserModel[];
+    searchUsers(query: string, page?: number, limit?: number): UserModel[];
     addUser(user: UserModel): void;
 }
