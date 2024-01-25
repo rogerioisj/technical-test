1d6db57ce2180154f31aec4e9bca13991a3871ba
Rogerio Inacio <rogerio.inacio@u4crypto.com>
2024-01-24 23:00:51 -0300

    feat: add controller

--- src/index.ts
+++ src/index.ts
@@ -4,6 +4,7 @@ import { UserRepository } from "./database/repositories/user.repository";
 import { GetUsersService } from "./useCases/get-users.service";
 import { AddUsersService } from "./useCases/add-users.service";
 import { ProcessCsvFileService } from "./useCases/process-csv-file.service";
+import { AppController } from './controllers/app.controller';
 
 const server = new ExpressServer();
 
@@ -12,8 +13,10 @@ const getUsersService = new GetUsersService(userRepository);
 const addUserService = new AddUsersService(userRepository);
 const processCsvFileService = new ProcessCsvFileService(addUserService);
 const userController = new UserController(getUsersService, processCsvFileService);
+const appController = new AppController();
 
 server.setRoutes(userController.getRoutes(), '/api');
+server.setRoutes(appController.getRoutes(), '/api');
 
 server.setPort(3000)
 server.start();
